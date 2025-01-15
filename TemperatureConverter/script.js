4ba9fcf07e1f27682e1f0f1ca21082ed1b81e741
Kami Joseph <josephkiarie561@gmail.com>
2025-01-15 20:14:30 +0300

    Temperature Converter Complete

--- TemperatureConverter/script.js
+++ TemperatureConverter/script.js
@@ -2,16 +2,22 @@
 const textBox = document.getElementById("textBox");
 const toFahrenheit = document.getElementById("toFahrenheit");
 const tocelsius = document.getElementById("toCelsius");
+const results = document.getElementById("results");
 let temp;
 
 function convert(){
-    if(toFahrenheit.checked){
 
+    if(toFahrenheit.checked){
+        temp = Number(textBox.value);
+        temp = temp * 9 / 5 + 32;
+        results.textContent = temp.toFixed(1) + " Fahrenheit.";
     }
     else if(tocelsius.checked){
-
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5/9);
+        results.textContent = temp.toFixed(1) + " Celsius.";
     }
     else{
-        result.textContent = "Select a Unit";
+        results.textContent = "Select a Unit";
     }
 }
