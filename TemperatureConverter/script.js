6c4bb11dbf25ec38797961bc3449fa9d07912702
Kami Joseph <josephkiarie561@gmail.com>
2025-01-15 19:32:27 +0300

    CnvertFunc

--- TemperatureConverter/script.js
+++ TemperatureConverter/script.js
@@ -1,3 +1,17 @@
 // Simple Temperature converter Webapplication
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const tocelsius = document.getElementById("toCelsius");
+let temp;
 
-function convert() {}
+function convert(){
+    if(toFahrenheit.checked){
+
+    }
+    else if(tocelsius.checked){
+
+    }
+    else{
+        result.textContent = "Select a Unit";
+    }
+}
