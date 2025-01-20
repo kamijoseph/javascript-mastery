27fd8eeaa820d0b1e5d973ccf747ac62804974b3
Kami Joseph <josephkiarie561@gmail.com>
2025-01-20 18:50:52 +0300

    jrfjrfjrhfjr

--- PasswordGenerator/script.js
+++ PasswordGenerator/script.js
@@ -4,12 +4,29 @@ function generatePassword(lenght, includeLowercase, includeUppercase, includeNum
     const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
     const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const numberChars = "0123456789";
-    const symbolChars = "!@#$%^&*()+?<>~`/";
+    const symbolChars = "!@#$%^&*()+?<>~-_";
 
     let allowedChars = "";
     let password = "";
 
-    return '';
+    allowedChars += includeLowercase ? lowercaseChars: "";
+    allowedChars += includeUppercase ? uppercaseChars: "";
+    allowedChars += includeNumbers ? numberChars: "";
+    allowedChars += includeSymbols ? symbolChars: "";
+
+    if (lenght < 4){
+        return `(Password length must be above 4 characters)`;
+    }
+    if (allowedChars === 0){
+        return `Atleast one set of characters should be selected`;
+    }
+
+    for (let i = 0; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+
+    return password;
 }
 
 const passwordLength = 12;
@@ -19,4 +36,4 @@ const includeNumbers = true;
 const includeSymbols = true;
 
 const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
-console.log(`Generated Password: ${password}`)
+console.log(`Generated Password: ${password}`);
