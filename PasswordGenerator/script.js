82d8c759d28db4cfcb9c2a2b8c2e69bea110ccc4
Kami Joseph <josephkiarie561@gmail.com>
2025-01-20 17:57:53 +0300

    Points initialized

---
+++ PasswordGenerator/script.js
@@ -0,0 +1,15 @@
+
+// Random Password Generator
+function generatePassword(lenght, includeLowercase, includeUppercase, includeNumbers, includeSymbols){
+
+    return '';
+}
+
+const passwordLength = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+console.log(`Generated Password: ${password}`)
