59203f29f59a8c67813819fea6189c802a7c8ee0
Kami Joseph <josephkiarie561@gmail.com>
2025-01-27 19:50:20 +0300

    simple color flipper complete

--- ColorFlipper/main.js
+++ ColorFlipper/main.js
@@ -1,4 +1,15 @@
 
-// color flipper script
-const colors = ["green", "red", "rgba(133, 122, 200), #f15025"]
-const btn = document.getElementById("btn")
+// script for main
+const colors = ["green", "red", "rgba(133, 122, 200, 0.8)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function() {
+    const randomNumber = getRandom();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+function getRandom(){
+    return randomNumber = Math.floor(Math.random() * colors.length);
+}
