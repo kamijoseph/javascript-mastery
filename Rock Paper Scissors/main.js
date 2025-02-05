20fbcfb28c2855b0c08f62172ca29171684bac07
Kami Joseph <josephkiarie561@gmail.com>
2025-02-05 21:15:49 +0300

    YOU LOSE, YOU WIN

--- Rock Paper Scissors/main.js
+++ Rock Paper Scissors/main.js
@@ -29,4 +29,13 @@ function playGame(playerChoice){
     playerDisplay.textContent = `PLAYER: ${playerChoice}`;
     computerDisplay.textContent = `PLAYER: ${computerChoice}`;
     resultDisplay.textContent = result;
+
+    switch(result){
+        case "YOU WIN!":
+            resultDisplay.classList.add("greenText");
+            break;
+        case "YOU LOSE!":
+            resultDisplay.classList.add("redText");
+            break;
+    }
 }
