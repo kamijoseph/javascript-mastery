5e26faf74b1157d17421d3220d99aff94699322f
Kami Joseph <josephkiarie561@gmail.com>
2025-02-05 20:22:19 +0300

    inevitability

--- Rock Paper Scissors/main.js
+++ Rock Paper Scissors/main.js
@@ -1,2 +1,10 @@
 
 // rock paper scissors javascript
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+function playGame(playerChoice){
+    const computerChoice =choices[Math.floor(Math.random() * 3)]
+}
