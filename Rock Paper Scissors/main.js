733d8ff6aa66ccf8a8cbf70b9a49e2fe1c4ccbdf
Kami Joseph <josephkiarie561@gmail.com>
2025-02-05 22:26:51 +0300

    scores script

--- Rock Paper Scissors/main.js
+++ Rock Paper Scissors/main.js
@@ -4,6 +4,10 @@ const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
 
 function playGame(playerChoice){
     const computerChoice =choices[Math.floor(Math.random() * 3)];
@@ -35,9 +39,13 @@ function playGame(playerChoice){
     switch(result){
         case "YOU WIN!":
             resultDisplay.classList.add("greenText");
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore
             break;
         case "YOU LOSE!":
             resultDisplay.classList.add("redText");
+            computerScore++;
+            computerScoreDisplay.textContent = computerScore
             break;
     }
 }
