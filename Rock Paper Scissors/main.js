22124a18f14a043efc56a32e3c783290f5bf1509
Kami Joseph <josephkiarie561@gmail.com>
2025-02-05 20:50:44 +0300

    player computer display jscript

--- Rock Paper Scissors/main.js
+++ Rock Paper Scissors/main.js
@@ -8,7 +8,24 @@ const resultDisplay = document.getElementById("resultDisplay");
 function playGame(playerChoice){
     const computerChoice =choices[Math.floor(Math.random() * 3)];
     let result = "";
+
     if (playerChoice === computerChoice){
         result = "ITS A TIE!";
-    };
+    }
+    else{
+        switch(playerChoice){
+            case "rock":
+                result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "paper":
+                result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "scissors":
+                result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+        }
+    }
+
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `PLAYER: ${computerChoice}`;
 }
