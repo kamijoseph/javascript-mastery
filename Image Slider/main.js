995f492cbc7811b4f113e9c9010926957b0a27bd
Kami Joseph <josephkiarie561@gmail.com>
2025-02-06 17:49:23 +0300

    script initialized

--- Image Slider/main.js
+++ Image Slider/main.js
@@ -1,2 +1,21 @@
 
 // image slider script
+const slides = document.querySelectorAll('.slides img');
+let slideIndex = 0;
+let intervalId = null;
+
+function initializeSlider(){
+
+}
+
+function showSlide(){
+
+}
+
+function prevSlide(){
+    
+}
+
+function nextSlide(){
+    
+}
